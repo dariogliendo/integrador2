f26ed7f50aa7e4c3d46534b614450c7671f41f04
dliendo <dliendo@somoswoxi.com>
2023-06-29 23:03:09 -0300

    implementacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NewsGrid from './Components/NewsGrid';
+import NewsDetail from './Components/NewsDetail';
+import Root from './Components/Root';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+      {
+        path: '',
+        element: <NewsGrid section="ultimas-noticias"/>
+      },
+      {
+        path: 'primera',
+        element: <NewsGrid section="futbol-primera"/>
+      },
+      {
+        path: 'copa-argentina',
+        element: <NewsGrid section="copa-argentina"/>
+      },
+      {
+        path: 'internacional',
+        element: <NewsGrid section="futbol-internacional"/>
+      },
+      {
+        path: 'libertadores',
+        element: <NewsGrid section="futbol-internacional/libertadores"/>
+      },
+      {
+        path: 'basquet',
+        element: <NewsGrid section="basquet"/>
+      },
+      {
+        path: 'boxeo',
+        element: <NewsGrid section="boxeo"/>
+      },
+      {
+        path: 'article/:url',
+        element: <NewsDetail/>
+      }
+    ]
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <RouterProvider router={router}/>
+    </React.StrictMode>
   );
 }
 
